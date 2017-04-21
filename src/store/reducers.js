24ed2b6deecdc0bbd08186b7d7adee8bf7426877
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-04-21 17:09:06 +0300

    create array reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,10 +2,22 @@ import C from '../constants'
 
 export const goal = (state = 10, action) =>
   (action.type === C.SET_GOAL) ?
-    action.payload :
+    parseInt(action.payload) :
     state
 
 export const skiDay = (state = null, action) => 
   (action.type === C.ADD_DAY) ?
     action.payload :
     state
+
+export const errors = (state = [], action) => {
+  switch (action.type) {
+    case C.ADD_ERROR:
+      return [
+        ...state,
+        action.payload
+      ]
+    default: 
+      return state  
+  }
+}
