4d3c5d5a1975c1bf952d95c65f69039630af5d61
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-04-27 19:21:07 +0300

    create middleware

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -15,8 +15,8 @@ export const errors = (state=[], action) => {
   switch(action.type) {
     case C.ADD_ERROR :
       return [
-         ...state,
-         action.payload
+        ...state,
+        action.payload
       ]
     case C.CLEAR_ERROR : 
       return state.filter((message, i) => i !== action.payload)
@@ -31,10 +31,10 @@ export const allSkiDays = (state = [], action) => {
       const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
       return (hasDay) ?
          state :
-         [
-           ...state,
-           skiDay(null, action)
-         ].sort((a, b) => new Date(b.date) - new Date(a.date))
+          [
+            ...state,
+            skiDay(null, action)
+          ].sort((a, b) => new Date(b.date) - new Date(a.date))
     case C.REMOVE_DAY :
       return state.filter(skiDay => skiDay.date !== action.payload)     
     default:
