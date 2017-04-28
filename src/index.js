ce4e4a8dbe072df551a6ccbb4cc76dab808cd571
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-04-28 15:53:30 +0300

    autocomplete thunk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import storeFactory from './store' // actually this is ./store/index.js
-import expect from 'expect'
-import { randomGoals } from './actions'
+import { suggestResortNames } from './actions'
+
 
 const store = storeFactory()
 
 store.dispatch(
-  randomGoals()
+  suggestResortNames('hea')
 )
