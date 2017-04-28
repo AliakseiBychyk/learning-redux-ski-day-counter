16b7586353b1b3fdbc89ab23f8c03ab85a1c608a
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-04-28 13:06:54 +0300

    add action creators

---
+++ src/actions.js
@@ -0,0 +1,19 @@
+import C from './constants'
+
+export function addDay(resort, date, powder = false, backcountry = false) {
+  return {
+    type: C.ADD_DAY,
+    payload: { resort, date, powder, backcountry }
+  }
+}
+
+export const removeDay = date => ({
+    type: C.REMOVE_DAY,
+    payload: date
+  })
+
+
+export const setGoal = goal => ({
+    type: C.SET_GOAL,
+    payload: goal
+  })
--- src/index.js
+++ src/index.js
@@ -1,45 +1,16 @@
-import C from './constants'
 import storeFactory from './store' // actually this is ./store/index.js 
+import { addDay, removeDay, setGoal } from './actions'
 
-const initialState = (localStorage['redux-store']) ?
-  JSON.parse(localStorage['redux-store']) :
-  {}
+const store = storeFactory()
 
-const saveState = () => {
-  const state = JSON.stringify(store.getState())
-  localStorage['redux-store'] = state
-}
+store.dispatch(
+  addDay('Heavenly', '2016-12-22')
+)
 
-const store = storeFactory(initialState)
+store.dispatch(
+  removeDay('2016-12-22')
+)
 
-store.subscribe(saveState)
-
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    "resort": "Mt Shasta",
-    "date": "2016-10-26",
-    "powder": true,
-    "backcountry": true
-  }
-})
-
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    "resort": "Squaw Valley ",
-    "date": "2016-4-26",
-    "powder": true,
-    "backcountry": false
-  }
-})
-
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    "resort": "The Canyons",
-    "date": "2016-1-20",
-    "powder": false,
-    "backcountry": true
-  }
-})
+store.dispatch(
+  setGoal(55)
+)
